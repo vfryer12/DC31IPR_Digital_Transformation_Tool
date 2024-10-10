8bf2baaef554ce6b526467e48080d68c63cc7366
victoria <vfryer12@hotmail.co.uk>
2024-10-11 00:46:11 +0100

    [DIG-118b]Fixed login

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-# run.py
-import os
-import sys
-
-file_dir = os.path.dirname(__file__)
-sys.path.append(file_dir)
-
-from .  import create_app
+# app.py
+from route_utils import create_app
 
 app = create_app()
 

4892cc0cb6c691add057f71402ea2c4c27330618
victoria <vfryer12@hotmail.co.uk>
2024-11-14 23:35:24 +0000

    [DIG-124] Finished mapping answers in page one controller

---
+++ controllers/utils/mappings_page_one.py
@@ -0,0 +1,119 @@
+# utils/mappings.py
+
+# Mapping for question one
+answer_map_q1 = {
+    '1': 'No plan in place',
+    '2': 'Needs Improvement',
+    '3': 'Fully Aligned'
+}
+
+# Mapping for question two
+answer_map_q2 = {
+    'cio': 'Chief Information Officer (CIO)',
+    'cto': 'Chief Technology Officer (CTO)',
+    'dtm': 'Digital Transformation Manager',
+    'cdo': 'Chief Digital Officer (CDO)',
+    'itd': 'IT Director/Manager',
+    'bul': 'Business Unit Leader',
+    'md': 'Marketing Director',
+    'dal': 'Data Analytics Leader',
+    'other': 'Other'
+}
+
+# Mapping for question three
+answer_map_q3 = {
+    'never': 'Never',
+    'monthly': 'Every Month',
+    'quarterly': 'Quarterly',
+    'year': 'Annually',
+    'continuous': 'Continuous'
+}
+
+# Mapping for question four
+answer_map_q4 = {
+    'return-on-investment-answer': 'Return on Investment (ROI)',
+    'conversion-rates-answer': 'Conversion Rates',
+    'customer-acquisition-cost-and-customer-lifetime-value-answer': 'Customer Acquisition Cost (CAC) and Customer Lifetime Value (CLV)',
+    'website-traffic-answer': 'Website Traffic',
+    'customer-engagement-answer': 'Customer Engagement',
+    'social-media-metrics-and-email-marketing-metrics-answer': 'Social Media Metrics and Email Marketing Metrics',
+    'seo-rankings-answer': 'SEO Rankings'
+}
+
+# Mapping for question five
+answer_map_q5 = {
+    'csat-metrics-answer': 'Customer Satisfaction Score (CSAT)',
+    'ces-metrics-answer': 'Customer Effort Score (CES)',
+    'digital-investments-answer': 'Return on Digital Investments',
+    'employee-productivity-answer': 'Employee Productivity',
+    'adoption-performance-answer': 'Adoption Performance Metrics',
+    'customer-experience-metrics': 'Customer Experience Metrics',
+    'percentage-ai-business-answer': 'Percentage of AI-enabled Businesses',
+    'reliability-availability-answer': 'Reliability and Availability',
+    'cost-benefit-answer': 'Cost-Benefit Analysis',
+    'revenue-digital-tech-answer': 'Revenue from Digital Technology',
+    'percent-cloud-deploy-answer': 'Percentage of Cloud Deployments',
+    'active-usage-metric-answer': 'Active Usage Metrics',
+    'conversion-rate-answer': 'Conversion Rate',
+    'customer-value-answer': 'Customer Lifetime Value',
+    'website-traffic-answer': 'Website Traffic',
+    'social-engagement-answer': 'Social Media Engagement',
+    'brand-engagement-answer': 'Brand Engagement On Website',
+    'traffic-sources-answer': 'Traffic Sources',
+    'landing-page-answer': 'Landing Page Conversions',
+    'inbound-marketing-answer': 'Inbound Marketing ROI',
+    'social-traffic-answer': 'Social Media Traffic'
+}
+
+# Mapping for question six
+answer_map_q6 = { 
+    'clear-communication-answer': 'Clear Communication',
+    'engagement-answer': 'Engagement',
+    'training-answer': 'Training',
+    'feedback-answer': 'Feedback',
+    'leadership-answer': 'Leadership',
+    'incentives-answer': 'Incentives',
+    'monitoring-answer': 'Monitoring'
+}
+
+# Mapping for question seven
+answer_map_q7 = { 
+    'alignment-answer': 'Alignment',
+    'resistance-to-change-answer': 'Resistance to Change',
+    'skill-gaps-answer': 'Skill Gaps',
+    'resource-allocation-answer': 'Resource Allocation',
+    'data-management-answer': 'Data Management',
+    'keeping-pace-answer': 'Keeping Pace',
+    'measuring-success-answer': 'Measuring Success'
+}
+
+# Mapping for question eight
+answer_map_q8 = { 
+    'feedback-mechanisms-answer': 'Feedback Mechanisms',
+    'data-analysis-answer': 'Data Analysis',
+    'regular-reviews-answer': 'Regular Reviews',
+    'adaptability-answer': 'Adaptability',
+    'communication-answer': 'Communication',
+    'documentation-answer': 'Documentation'
+}
+
+# Mapping for question nine
+answer_map_q9 = { 
+    'strategic-fit-answer': 'Strategic Fit',
+    'research-answer': 'Research',
+    'risk-assessment-answer': 'Risk Assessment',
+    'pilot-projects-answer': 'Pilot Projects',
+    'training-answer': 'Training',
+    'scalability-answer': 'Scalability'
+}
+
+# Mapping for question ten
+answer_map_q10 = { 
+    'understanding-needs-answer': 'Understanding Needs',
+    'personalization-answer': 'Personalization',
+    'seamless-experience-answer': 'Seamless Experience',
+    'customer-feedback-answer': 'Customer Feedback',
+    'engagement-answer': 'Engagement',
+    'measurement-answer': 'Measurement'
+}
+

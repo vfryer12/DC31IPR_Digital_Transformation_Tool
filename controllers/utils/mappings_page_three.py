220fa459c6c9fdc1e332a448bdb7444f142f2fc6
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2024-11-18 22:15:41 +0000

    [DIG-130a] Created page three controller (#35)

---
+++ controllers/utils/mappings_page_three.py
@@ -0,0 +1,142 @@
+# utils/mappings_page_three.py
+
+# Mappings for page three question one
+answer_map_page_three_q1 = { 
+    'define-clear-objectives-answer': 'Define Clear Objectives',
+    'assess-alignment-with-your-business-answer': 'Assess Alignment with Your Business',
+    'feasibility-and-viability-check-answer': 'Feasibility and Viability Check',
+    'risk-analysis-answer': 'Risk Analysis',
+    'performance-against-KPIs-answer': 'Performance Against KPIs',
+    'test-and-iterate-answer': 'Test and Iterate',
+    'explore-market-potential-answer': 'Explore Market Potential',
+    'collaborate-and-seek-expert-input-answer': 'Collaborate and Seek Expert Input',
+    'user-friendliness-answer': 'User-Friendliness',
+    'long-term-vision-assessmentg-answer': 'Long-Term Vision Assessment'
+}
+
+# Mapping for page three question two
+answer_map_page_three_q2 = { 
+    'clear-communication-answer': 'Clear Communication',
+    'leadership-support-answer': 'Leadership Support',
+    'comprehensive-training-programs-answer': 'Comprehensive Training Programs',
+    'change-management-strategies-answer': 'Change Management Strategies',
+    'user-friendly-design-answer': 'User-Friendly Design',
+    'support-systems-answer': 'Support Systems',
+    'pilot-testing-answer': 'Pilot Testing',
+    'gradual-implementation-answer': 'Gradual Implementation',
+    'feedback-mechanisms-answer': 'Feedback Mechanisms',
+    'monitor-and-evaluate-answer': 'Monitor and Evaluate'
+}
+
+# Mapping for page three question three
+answer_map_page_three_q3 = { 
+    'resistance-to-change-answer': 'Resistance to Change',
+    'lack-of-skilled-personnel-answer': 'Lack of Skilled Personnel',
+    'complexity-of-technology-answer': 'Complexity of Technology',
+    'security-concerns-answer': 'Security Concerns',
+    'budget-constraints-answer': 'Budget Constraints',
+    'inadequate-training-answer': 'Inadequate Training',
+    'cultural-mindset-answer': 'Cultural Mindset',
+    'siloed-organizational-structure-answer': 'Siloed Organizational Structure',
+    'continuous-evolution-of-customer-needs-answer': 'Continuous Evolution of Customer Needs',
+    'measuring-ROI-answer': 'Measuring ROI'
+}
+
+# Mapping for page three question four
+answer_map_page_three_q4 = { 
+    'user-adoption-rate-answer': 'User Adoption Rate',
+    'performance-metrics-answer': 'Performance Metrics',
+    'return-on-investment-answer': 'Return on Investment (ROI)',
+    'user-satisfaction-answer': 'User Satisfaction',
+    'usage-frequency-answer': 'Usage Frequency',
+    'business-impact-answer': 'Business Impact',
+    'time-to-proficiency-answer': 'Time to Proficiency',
+    'support-requests-answer': 'Support Requests',
+    'feature-adoption-rate-answer': 'Feature Adoption Rate',
+    'training-completion-rates-answer': 'Training Completion Rates'
+}
+
+
+# Mapping for page three question five
+answer_map_page_three_q5 = { 
+    'define-clear-business-goals-answer': 'Define Clear Business Goals',
+    'collaborate-with-stakeholders-answer': 'Collaborate with Stakeholders',
+    'conduct-a-needs-assessment-answer': 'Conduct a Needs Assessment',
+    'develop-a-unified-strategy-answer': 'Develop a Unified Strategy',
+    'monitor-and-measure-performance-answer': 'Monitor and Measure Performance',
+    'data-driven-decision-making-answer': 'Data-Driven Decision Making',
+    'implement-governance-and-accountability-answer': 'Implement Governance and Accountability',
+    'continuous-feedback-loops-answer': 'Continuous Feedback Loops',
+    'encourage-cross-functional-collaboration-answer': 'Encourage Cross-Functional Collaboration',
+    'adapt-and-evolve-answer': 'Adapt and Evolve'
+}
+
+# Mapping for page three question six
+answer_map_page_three_q6 = { 
+    'set-and-communicate-clear-goals-answer': 'Set and Communicate Clear Goals',
+    'develop-a-comprehensive-strategy-answer': 'Develop a Comprehensive Strategy',
+    'provide-continuous-training-and-support-answer': 'Provide Continuous Training and Support',
+    'build-a-dedicated-change-management-team-answer': 'Build a Dedicated Change Management Team',
+    'plan-for-resistance-answer': 'Plan for Resistance',
+    'encourage-employee-involvement-answer': 'Encourage Employee Involvement',
+    'monitor-and-evaluate-progress-answer': 'Monitor and Evaluate Progress',
+    'communicate-regularly-answer': 'Communicate Regularly',
+    'foster-a-culture-of-innovationn-answer': 'Foster a Culture of Innovation',
+    'celebrate-successes-answer': 'Celebrate Successes'
+}
+
+# Mapping for page three question seven
+answer_map_page_three_q7 = { 
+    'join-professional-organizations-answer': 'Join Professional Organizations',
+    'attend-industry-conferences-and-tech-events-answer': 'Attend Industry Conferences and Tech Events',
+    'follow-tech-news-and-blogs-answer': 'Follow Tech News and Blogs',
+    'engage-on-social-media-answer': 'Engage on Social Media',
+    'take-online-courses-and-webinars-answer': 'Take Online Courses and Webinars',
+    'network-with-peers-answer': 'Network with Peers',
+    'find-a-mentor-answer': 'Find a Mentor',
+    'watch-TED-talks-and-tech-videos-answer': 'Watch TED Talks and Tech Videos',
+    'participate-in-online-forums-and-communities-answer': 'Participate in Online Forums and Communities',
+    'read-industry-reports-and-whitepapers-answer': 'Read Industry Reports and Whitepapers'
+}
+
+# Mapping for page three question eight
+answer_map_page_three_q8 = { 
+    'regularly-review-and-update-answer': 'Regularly Review and Update',
+    'stay-informed-on-industry-trends-answer': 'Stay Informed on Industry Trends',
+    'implement-continuous-integration-and-deployment-answer': 'Implement Continuous Integration and Deployment (CI/CD)',
+    'conduct-regular-audits-answer': 'Conduct Regular Audits',
+    'engage-with-the-developer-community-answer': 'Engage with the Developer Community',
+    'foster-a-culture-of-learning-answer': 'Foster a Culture of Learning',
+    'collaborate-with-vendors-and-partners-answer': 'Collaborate with Vendors and Partners',
+    'adopt-modular-architecture-answer': 'Adopt Modular Architecture',
+    'monitor-and-analyze-performance-answer': 'Monitor and Analyze Performance',
+    'plan-for-scalability-and-future-growth-answer': 'Plan for Scalability and Future Growth'
+}
+
+# Mapping for page three question nine
+answer_map_page_three_q9 = { 
+    'conduct-a-comprehensive-risk-assessment-answer': 'Conduct a Comprehensive Risk Assessment',
+    'develop-a-risk-mitigation-plan-answer': 'Develop a Risk Mitigation Plan',
+    'perform-a-cost-benefit-analysis-answer': 'Perform a Cost-Benefit Analysis',
+    'conduct-cybersecurity-assessments-answer': 'Conduct Cybersecurity Assessments',
+    'implement-pilot-testing-answer': 'Implement Pilot Testing',
+    'monitor-regulatory-compliance-answer': 'Monitor Regulatory Compliance',
+    'engage-with-stakeholders-answer': 'Engage with Stakeholders',
+    'analyze-historical-data-answer': 'Analyze Historical Data',
+    'use-risk-assessment-tools-answer': 'Use Risk Assessment Tools',
+    'seek-expert-consultation-answer': 'Seek Expert Consultation'
+}
+
+# Mapping for page three question ten
+answer_map_page_three_q10 = { 
+    'communicate-the-benefits-answer': 'Communicate the Benefits',
+    'involve-employees-early-answer': 'Involve Employees Early',
+    'provide-comprehensive-training-answer': 'Provide Comprehensive Training',
+    'address-concerns-openly-answer': 'Address Concerns Openly',
+    'implement-change-management-strategies-answer': 'Implement Change Management Strategies',
+    'foster-a-positive-culture-answer': 'Foster a Positive Culture',
+    'provide-ongoing-support-answer': 'Provide Ongoing Support',
+    'showcase-success-stories-answer': 'Showcase Success Stories',
+    'offer-incentive-answer': 'Offer Incentive',
+    'monitor-and-adjust-answer': 'Monitor and Adjust'
+}

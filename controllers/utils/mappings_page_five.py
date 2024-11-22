639a39aad0d26ad99215ccae419264652787e0c2
victoria <vfryer12@hotmail.co.uk>
2024-11-22 22:57:23 +0000

    [DIG-135]Created controller number five

---
+++ controllers/utils/mappings_page_five.py
@@ -0,0 +1,154 @@
+#  Mapping for page five question one
+answer_map_page_five_q1 = {
+    'search-engine-optimization-answer': 'Search Engine Optimization (SEO)',
+    'social-media-marketing-answer': 'Social Media Marketing',
+    'content-marketing-answer': 'Content Marketing',
+    'pay-per-click-advertising-answer': 'Pay-Per-Click (PPC) Advertising',
+    'email-marketing-answer': 'Email Marketing',
+    'influencer-marketing-answer': 'Influencer Marketing',
+    'video-marketing-answer': 'Video Marketing',
+    'retargeting-campaigns-answer': 'Retargeting Campaigns',
+    'affiliate-marketing-answer': 'Affiliate Marketing',
+    'webinars-and-live-streams-answer': 'Webinars and Live Streams'
+}
+
+# Mapping for page five question two
+answer_map_page_five_q2 = {
+    'return-on-investment-answer': 'Return on Investment (ROI)',
+    'conversion-rate-answer': 'Conversion Rate',
+    'customer-acquisition-cost-answer': 'Customer Acquisition Cost (CAC)',
+    'customer-lifetime-value-answer': 'Customer Lifetime Value (CLV)',
+    'website-traffic-answer': 'Website Traffic',
+    'lead-generation-answer': 'Lead Generation',
+    'engagement-metrics-answer': 'Engagement Metrics',
+    'social-media-metrics-answer': 'Social Media Metrics',
+    'email-open-and-click-through-rates-answer': 'Email Open and Click-Through Rates',
+    'bounce-rate-answer': 'Bounce Rate'
+}
+
+# Mapping for page five question three
+answer_map_page_five_q3 = {
+    'utilizing-data-analytics-answer': 'Utilizing Data Analytics',
+    'enhancing-seo-practices-answer': 'Enhancing SEO Practices',
+    'expanding-social-media-presence-answer': 'Expanding Social Media Presence',
+    'investing-in-content-marketing-answer': 'Investing in Content Marketing',
+    'improving-email-marketing-campaigns-answer': 'Improving Email Marketing Campaigns',
+    'implementing-marketing-automation-answer': 'Implementing Marketing Automation',
+    'leveraging-influencer-partnerships-answer': 'Leveraging Influencer Partnerships',
+    'expanding-video-marketing-answer': 'Expanding Video Marketing',
+    'optimizing-mobile-marketing-answer': 'Optimizing Mobile Marketing',
+    'focusing-on-customer-experience-answer': 'Focusing on Customer Experience'
+}
+
+# Mapping for page five question four
+answer_map_page_five_q4 = {
+    'defining-goals-and-key-performance-indicators-answer': 'Defining Goals and Key Performance Indicators (KPIs)',
+    'analyzing-and-interpreting-data-answer': 'Analyzing and Interpreting Data',
+    'personalizing-marketing-campaigns-answer': 'Personalizing Marketing Campaigns',
+    'predictive-analytics-answer': 'Predictive Analytics',
+    'optimizing-marketing-channels-answer': 'Optimizing Marketing Channels',
+    'real-time-analytics-answer': 'Real-Time Analytics',
+    'a/b-testing-answer': 'A/B Testing',
+    'customer-journey-mapping-answer': 'Customer Journey Mapping',
+    'collecting-and-cleansing-data-answer': 'Collecting and Cleansing Data',
+    'reporting-and-visualization-answer': 'Reporting and Visualization'
+}
+
+# Mapping for page five question five
+answer_map_page_five_q5 = {
+    'keyword-research-answer': 'Keyword Research',
+    'on-page-optimization-answer': 'On-Page Optimization',
+    'content-creation-answer': 'Content Creation',
+    'technical-seo-answer': 'Technical SEO',
+    'link-building-answer': 'Link Building',
+    'user-experience-optimization-answer': 'User Experience (UX) Optimization',
+    'local-seo-answer': 'Local SEO',
+    'regular-audits-and-updates-answer': 'Regular Audits and Updates',
+    'analytics-and-reporting-answer': 'Analytics and Reporting',
+    'competitor-analysis-answer': 'Competitor Analysis'
+}
+
+# Mapping for page five question six
+answer_map_page_five_q6 = {
+    'building-brand-awareness-answer': 'Building Brand Awareness',
+    'engaging-with-customers-answer': 'Engaging with Customers',
+    'running-paid-advertising-campaigns-answer': 'Running Paid Advertising Campaigns',
+    'content-promotion-answer': 'Content Promotion',
+    'providing-customer-support-answer': 'Providing Customer Support',
+    'influencer-collaborations-answer': 'Influencer Collaborations',
+    'social-listening-answer': 'Social Listening',
+    'analyzing-performance-metrics-answer': 'Analyzing Performance Metrics',
+    'hosting-contests-and-giveaways-answer': 'Hosting Contests and Giveaways',
+    'creating-a-content-calendar-answer': 'Creating a Content Calendar'
+}
+
+# Mapping for page five question six
+answer_map_page_five_q6 = {
+    'building-brand-awareness-answer': 'Building Brand Awareness',
+    'engaging-with-customers-answer': 'Engaging with Customers',
+    'running-paid-advertising-campaigns-answer': 'Running Paid Advertising Campaigns',
+    'content-promotion-answer': 'Content Promotion',
+    'providing-customer-support-answer': 'Providing Customer Support',
+    'influencer-collaborations-answer': 'Influencer Collaborations',
+    'social-listening-answer': 'Social Listening',
+    'analyzing-performance-metrics-answer': 'Analyzing Performance Metrics',
+    'hosting-contests-and-giveaways-answer': 'Hosting Contests and Giveaways',
+    'creating-a-content-calendar-answer': 'Creating a Content Calendar'
+}
+
+# Mapping for page five question seven
+answer_map_page_five_q7 = {
+    'personalized-campaigns-answer': 'Personalized Campaigns',
+    'automated-email-sequences-answer': 'Automated Email Sequences',
+    'promotional-emails-answer': 'Promotional Emails',
+    'abandoned-cart-emails-answer': 'Abandoned Cart Emails',
+    'newsletters-answer': 'Newsletters',
+    'transactional-emails-answer': 'Transactional Emails',
+    're-engagement-campaigns-answer': 'Re-engagement Campaigns',
+    'event-invitations-answer': 'Event Invitations',
+    'surveys-and-feedback-requests-answer': 'Surveys and Feedback Requests',
+    'educational-content-answer': 'Educational Content'
+}
+
+# Mapping for page five question eight
+answer_map_page_five_q8 = {
+    'creating-valuable-blog-content-answer': 'Creating Valuable Blog Content',
+    'seo-optimization-answer': 'SEO Optimization',
+    'developing-video-content-answer': 'Developing Video Content',
+    'utilizing-social-media-answer': 'Utilizing Social Media',
+    'publishing-ebooks-and-whitepapers-answer': 'Publishing Ebooks and Whitepapers',
+    'hosting-webinars-and-live-events-answer': 'Hosting Webinars and Live Events',
+    'email-newsletters-answer': 'Email Newsletters',
+    'creating-infographics-answer': 'Creating Infographics',
+    'podcasting-answer': 'Podcasting',
+    'user-generated-content-answer': 'User-Generated Content'
+}
+
+
+# Mapping for page five question nine
+answer_map_page_five_q9 = {
+    'partnering-with-relevant-influencers-answer': 'Partnering with Relevant Influencers',
+    'creating-authentic-content-answer': 'Creating Authentic Content',
+    'leveraging-micro-influencers-answer': 'Leveraging Micro-Influencers',
+    'tracking-and-analyzing-performance-answer': 'Tracking and Analyzing Performance',
+    'building-long-term-relationships-answer': 'Building Long-Term Relationships',
+    'utilizing-influencer-reviews-and-testimonials-answer': 'Utilizing Influencer Reviews and Testimonials',
+    'creating-sponsored-content-answer': 'Creating Sponsored Content',
+    'hosting-giveaways-and-contests-answer': 'Hosting Giveaways and Contests',
+    'running-influencer-takeovers-answer': 'Running Influencer Takeovers',
+    'utilizing-influencer-content-across-channels-answer': 'Utilizing Influencer Content Across Channels'
+}
+
+# Mapping for page five question ten
+answer_map_page_five_q10 = {
+    'product-demonstrations-answer': 'Product Demonstrations',
+    'customer-testimonials-answer': 'Customer Testimonials',
+    'educational-content-answer': 'Educational Content',
+    'video-ads-answer': 'Video Ads',
+    'social-media-videos-answer': 'Social Media Videos',
+    'live-streaming-answer': 'Live Streaming',
+    'explainer-videos-answer': 'Explainer Videos',
+    'behind-the-scenes-footage-answer': 'Behind-the-Scenes Footage',
+    'user-generated-content-answer': 'User-Generated Content',
+    'interactive-videos-answer': 'Interactive Videos'
+}

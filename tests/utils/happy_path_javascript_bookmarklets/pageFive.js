7a6f6182edbe9db128fbd24252d7de9a9f55ab01
victoria <vfryer12@hotmail.co.uk>
2025-01-02 22:46:46 +0000

    [DIG-155]Added javascript bookmarklets

---
+++ tests/utils/happy_path_javascript_bookmarklets/pageFive.js
@@ -0,0 +1,24 @@
+//PAGE FIVE HAPPY PATH
+
+javascript:(function() {
+	document.getElementById('search-engine-optimization-answer').checked = true;
+	document.getElementById('social-media-marketing-answer').checked = true;
+	document.getElementById('content-marketing-answer').checked = true;
+	document.getElementById('return-on-investment-answer').checked = true;
+	document.getElementById('conversion-rate-answer').checked = true;
+	document.getElementById('customer-acquisition-cost-answer').checked = true;
+	document.getElementById('utilizing-data-analytics-answer').checked = true;
+	document.getElementById('enhancing-seo-practices-answer').checked = true;
+	document.getElementById('expanding-social-media-presence-answer').checked = true;
+	document.getElementById('defining-goals-and-key-performance-indicators-answer').checked = true;
+	document.getElementById('keyword-research-answer').checked = true;
+	document.getElementById('building-brand-awareness-answer').checked = true;
+	document.getElementById('engaging-with-customers-answer').checked = true;
+	document.getElementById('running-paid-advertising-campaigns-answer').checked = true;
+	document.getElementById('personalized-campaigns-answer').checked = true;
+	document.getElementById('automated-email-sequences-answer').checked = true;
+	document.getElementById('promotional-emails-answer').checked = true;
+	document.getElementById('creating-valuable-blog-content-answer').checked = true;
+	document.getElementById('partnering-with-relevant-influencers-answer').checked = true;
+	document.getElementById('product-demonstrations-answer').checked = true;
+})();
---
+++ tests/utils/happy_path_javascript_bookmarklets/pageFour.js
@@ -0,0 +1,22 @@
+//PAGE FOUR HAPPY PATH
+
+javascript:(function() {
+	document.getElementById('market-research-and-analysis-answer').checked = true;
+	document.getElementById('customer-feedback-and-insights-answer').checked = true;
+	document.getElementById('networking-and-conferences-answer').checked = true;
+	document.getElementById('data-driven-decision-making-answer').checked = true;
+	document.getElementById('adopting-AI-and-machine-learning-answer').checked = true;
+	document.getElementById('investing-in-data-analytics-answer').checked = true;
+	document.getElementById('enhancing-cybersecurity-measures-answer').checked = true;
+	document.getElementById('leveraging-data-and-analytics-answer').checked = true;
+	document.getElementById('advanced-data-analytics-answer').checked = true;
+	document.getElementById('investing-in-emerging-technologies-q6-answer').checked = true;
+	document.getElementById('continuous-learning-and-development-answer').checked = true;
+	document.getElementById('agile-and-flexible-approach-answer').checked = true;
+	document.getElementById('data-driven-insights-answer').checked = true;
+	document.getElementById('personalization-and-customer-experience-answer').checked = true;
+	document.getElementById('adapting-to-technological-advancements-answer').checked = true;
+	document.getElementById('continuous-market-research-answer').checked = true;
+	document.getElementById('personalized-customer-interactions-answer').checked = true;
+	document.getElementById('identifying-emerging-markets-answer').checked = true;
+})();

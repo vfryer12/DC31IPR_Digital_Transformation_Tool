390645feb2c80639711317cb3edc1bca89e7623b
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2025-01-02 23:11:26 +0000

    [DIG-152]Updated registration form to be required (#51)

--- tests/unit/daos_unit_tests/get_db_data_test.py
+++ tests/unit/daos_unit_tests/get_db_data_test.py
@@ -2,7 +2,10 @@
 
 import pytest
 from unittest.mock import Mock, call
+from daos import mysql_queries
 from daos.get_db_data import get_section_answer_weights
+from textwrap import dedent
+
 
 def test_get_section_answer_weights_success():
     # Mock the database connection and cursor
@@ -10,54 +13,20 @@ def test_get_section_answer_weights_success():
     mock_cursor = Mock()
     mock_conn.cursor.return_value = mock_cursor
 
-    # Mock the SQL query results
-    expected_results = [("Section1", 1, 10), ("Section2", 2, 20)]
-    mock_cursor.fetchall.return_value = expected_results
+    # Mock the cursor methods
+    mock_cursor.column_names = ["section", "answersid", "weighting"]
+    mock_cursor.fetchall.return_value = [("Section1", 1, 10), ("Section2", 2, 20)]
 
     # Call the function with the mock connection and a test user_id
     user_id = 123
     result = get_section_answer_weights(mock_conn, user_id)
 
-     # Verify the results are as expected
-    assert result == expected_results
-    # Ensure the cursor was created
-    mock_conn.cursor.assert_called_once()
+    # Verify the results are as expected
+    assert result == [("Section1", 1, 10), ("Section2", 2, 20)]
     mock_cursor.execute.assert_called_once_with(
-        """WITH sectquest
-AS (
-	SELECT s.section
-		,q.id AS questionsId
-	FROM sections AS s
-	INNER JOIN questions AS q ON q.sectionid = s.id
-	)
-	,useransweight
-AS (
-	SELECT ua.answersId
-		,a.weighting
-		,a.questionsId
-	FROM useranswers AS ua
-	INNER JOIN answers AS a ON ua.answersId = a.id
-	WHERE ua.userid = %s
-	)
-	,section_answer_weight
-AS (
-	SELECT sq.section
-		,uw.answersid
-		,uw.weighting
-	FROM useransweight AS uw
-	INNER JOIN sectquest AS sq ON uw.questionsId = sq.questionsId
-	)
-SELECT section
-	,answersid
-	,weighting
-FROM section_answer_weight
-""",
-        [user_id],
+        dedent(mysql_queries.USER_ANSWER_WEIGHTS), [user_id]
     )
-    # Verify the query and parameters
-    # Ensure the cursor was closed
     mock_cursor.close.assert_called_once()
-
 
 def test_get_section_answer_weights_failure():
     # Mock the database connection and cursor
